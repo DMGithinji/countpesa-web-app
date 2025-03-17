4a0e3d75b7f10ddc8fb9e2320ec4e4dea6dd5ff1
Denis Githinji <dmwaigithinji@gmail.com>
2025-03-17 13:29:32 +0300

    add styling configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
   return (
-    <>
+    <div className='flex flex-col text-lg text-red-500 font-extrabold'>
       Click on the Vite and React logos to learn more
-    </>
+      <Button>Hello React + Vite + ShadCN</Button>
+    </div>
   )
 }
 
