663ca8f34746194972f35f87b0c68ad6f78e4670
Denis Githinji <dmwaigithinji@gmail.com>
2025-04-15 10:55:54 +0300

    cleaner import paths for filter utils

--- src/stores/transactions.store.ts
+++ src/stores/transactions.store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { Filter } from "@/types/Filters";
 import { Transaction } from "@/types/Transaction";
-import { removeFilters, validateAndAddFilters } from "@/lib/filterUtils/validateFilters";
+import { removeFilters, validateAndAddFilters, filterTransactions } from "@/lib/filterUtils";
 import {
   CalculatedData,
   DEFAULT_CALCULATED_DATA,
@@ -9,7 +9,6 @@ import {
 } from "@/lib/getCalculatedData";
 import { DateRangeData, DEFAULT_DATE_RANGE_DATA, getDateRangeData } from "@/lib/dateUtils";
 import { getAllAccountNames } from "@/lib/groupByField";
-import { filterTransactions } from "@/lib/filterUtils/filter";
 import { getPeriodAverages, PeriodAverages } from "@/lib/groupByPeriod";
 
 interface TransactionState {
